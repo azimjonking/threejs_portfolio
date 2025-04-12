caef507c356a1207e4e04d468db343fcc3692baf
azimjonking <azimjonjalilov1301@mail.ru>
2025-04-12 21:08:11 +0300

    navbar done

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <h1 className="text-white py-32">Hero</h1>
+    </section>
+  );
+};
+
+export default Hero;
